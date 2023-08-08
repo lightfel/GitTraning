0c981d0fcfe679fd9a9734452f1bc6c5da4e2bc3
lightfel <motomoto.long2222@gmail.com>
2023-08-08 23:58:07 +0900

    Initial Commit

---
+++ GitTraning/GitTraningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTraningApp.swift
+//  GitTraning
+//
+//  Created by Hikaru Nagatsuka on 2023/08/08.
+//
+
+import SwiftUI
+
+@main
+struct GitTraningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
