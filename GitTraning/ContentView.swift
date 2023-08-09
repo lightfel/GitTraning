0146c9b3e7ae84c2ba8cbff14a463b3310e02d86
Hiroaki Aoyama <bluemountainhiro@yahoo.co.jp>
2023-08-09 15:03:14 +0900

    Hi8/9に変更

--- GitTraning/ContentView.swift
+++ GitTraning/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!->Hi")
+            Text("Hello, world!->Hi8/9")
         }
         .padding()
     }
